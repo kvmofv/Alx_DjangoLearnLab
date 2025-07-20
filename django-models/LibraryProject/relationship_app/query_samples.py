adba8c67933a0ed5a092cf5198074fee7a9b1dcd
kvmofv <kvmofv@gmail.com>
2025-07-20 17:33:05 +0300

    Advanced Model relationships Task Final Update

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -16,5 +16,8 @@ library_name = 'Maddison'
 library = Library.objects.get(name=library_name)
 books = library.books.all()
 
+library_name = 'Maddison'
 library = Library.objects.get(name=library_name)
-librarian = library.librarian  
+
+librarian = Librarian.objects.get(library=library)
+
