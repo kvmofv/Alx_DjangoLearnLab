338a0a56183cc5f8a27a809abe62990c934462bb
kvmofv <kvmofv@gmail.com>
2025-07-20 20:04:21 +0300

    Implementing User Authentication in Django Task

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.detail import DetailView
