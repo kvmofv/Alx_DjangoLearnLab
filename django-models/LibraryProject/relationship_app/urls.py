b6b9396e2bfb77acbbc21e88cca9f6dad4cbe77d
kvmofv <kvmofv@gmail.com>
2025-07-20 23:13:21 +0300

    final task done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('admin_view/', views.admin_view, name= 'admin_view'),
     path('librarian_view/', views.librarian_view, name= 'librarian_view'),
     path('member_view/', views.member_view, name= 'member_view'),
+    path('delete_book/<int:pk>', views.delete_book, name= 'delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>', views.edit_book, name='edit_book'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import user_passes_test, permission_required
 from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.detail import DetailView
 from .models import Library, Book
+from .forms import BookForm
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created):
@@ -40,9 +41,36 @@ def is_member(user):
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list_books')
+    else:
+        form = BookForm()
+    return render(request, 'relationship_app/add_book.html', {"form": form})
 
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk = pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance = book)
+        if form.is_valid():
+            form.save()
+            return redirect('list_books')
+    else:
+        form = BookForm(instance = book)
+    return render(request, 'relationship_app/edit_book.html', {'form': form})
 
-
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk = pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('list_books')
+    return render(request, 'relationship_app/delete_book.html', {'form':form})
 
 
 
