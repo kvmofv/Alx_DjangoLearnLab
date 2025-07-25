9a3503315fe8a3fa104be9eaeafcecd0f63a05f2
kvmofv <kvmofv@gmail.com>
2025-07-26 00:24:20 +0300

    god help us

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role', 'date_of_birth', 'profile_photo')}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('role', 'date_of_birth', 'profile_photo')}),
+    )
+
+    list_display = ['username', 'email', 'role', 'is_staff', 'is_superuser']
+    list_filter = ['role', 'is_staff', 'is_superuser']
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,19 +1,1 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from bookshelf.models import CustomUser
-
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('role', 'date_of_birth', 'profile_photo')}),
-    )
-
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('role', 'date_of_birth', 'profile_photo')}),
-    )
-
-    list_display = ['username', 'email', 'role', 'is_staff', 'is_superuser']
-    list_filter = ['role', 'is_staff', 'is_superuser']
-
-admin.site.register(CustomUser, CustomUserAdmin)
