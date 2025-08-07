6b879ce43cfac421eeaf6278eb805d18e4d99fea
kvmofv <kvmofv@gmail.com>
2025-08-07 21:33:29 +0300

    Task 1 Update; Auto-checker _|_

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, filters
 from rest_framework.exceptions import ValidationError
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
