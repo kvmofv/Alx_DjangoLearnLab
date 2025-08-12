4e7f808053b458d559bd3cd02d6b6fb9acef21dd
kvmofv <kvmofv@gmail.com>
2025-08-12 21:59:52 +0300

    django blog Task1: Update

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -36,6 +36,11 @@ class RegistrationView(CreateView):
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'profile.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile'] = self.request.user.profile
+        return context
+
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'profile_edit.html'
 
