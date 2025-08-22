c6f333af1082d956fbc9fad3ff227bfa578c0282
kvmofv <kvmofv@gmail.com>
2025-08-23 01:00:50 +0300

    Task 3: updated posts/urls.py paths

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,5 +1,6 @@
+from django.urls import path
 from rest_framework_nested import routers
-from . views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, LikePostView, UnlikePostView
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
@@ -7,4 +8,7 @@ router.register(r'posts', PostViewSet, basename='post')
 posts_router = routers.NestedDefaultRouter(router, r'posts', lookup='post')
 posts_router.register(r'comments', CommentViewSet, basename='post-comments')
 
-urlpatterns = router.urls + posts_router.urls
+urlpatterns = router.urls + posts_router.urls + [
+    path('posts/<int:pk>/like/', LikePostView.as_view(), name='post-like'),
+    path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='post-unlike'),
+]
